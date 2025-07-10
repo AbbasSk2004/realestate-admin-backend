3a550df43115e1f3f039ccae38ce675c85e76591
AbbasSk2004 <abbasskaiki74@gmail.com>
2025-07-10 14:22:44 +0300

    limit

--- index.js
+++ index.js
@@ -47,8 +47,8 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Set secure HTTP headers
 app.use(helmet());
